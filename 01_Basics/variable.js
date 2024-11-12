66ba1b844a9c2bdbf6e16bfdddb05e247bb31f43
Bhavna patil <115781015+Bhavna-patil@users.noreply.github.com>
2024-11-12 10:53:03 +0000

    const,var,let,undefined

---
+++ 01_Basics/variable.js
@@ -0,0 +1,15 @@
+const studentId = 1234
+let studentName = "Bhavna" // mostly used
+var studentGrade = "A++" //old way to creat variable not use in modern programming bcoz of Scope issue
+
+institude = "TSEC" //another way but not preffered
+
+let studentPerformance;
+
+// studentId = 111 TypeError: Assignment to constant variable.
+
+console.table({studentId,studentName,studentGrade, institude})
+console.log(studentPerformance) //Undefined 
+
+
+
