f2c07e67f5be4b31a34b3b0ba42bac31cfe00769
Bhavna patil <115781015+Bhavna-patil@users.noreply.github.com>
2024-11-13 08:30:57 +0000

    DataTypes

---
+++ 01_Basics/dataType.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+
+// alert("hey") //work on browser we are using node js -_-
+
+/*
+
+  number => 2to the power
+  bigint => wide range of number
+  boolean => true/false
+  null => standalone value /intentionally assigned empty
+  undefined => not assigned any value
+  symbol => uniqueness
+
+*/
+
+let myIQ;
+let yourIQ = null
+console.log(typeof myIQ) // undefined
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
+console.log(typeof yourIQ); //object
+
+
