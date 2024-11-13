c625ed1772ecddacea0de40e31d7639f0e7d2b7b
Bhavna patil <115781015+Bhavna-patil@users.noreply.github.com>
2024-11-13 09:29:09 +0000

    conversion

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,30 @@
+"use strict"
+console.log(1/0) // Infinity
+console.log(0/0) // NaN
+console.log(0/1) // 0
+
+let num = "ab"
+let  NumberValue = Number(num)
+console.log(typeof NaN)
+console.log(typeof NumberValue)
+console.log(NumberValue)
+
+// "33" => 33
+//"33ab" => Nan // type of number which specify that the result of a number opration is not a number
+// true => 1, false=> 0 
+
+
+//coversion functions :-
+
+// Boolean()
+// String( )
+
+
+/*  *************************operation********************************             */
+
+// str+str = str;
+// str + number = str
+// "1"+2+3 = 123
+// 8+"2"+3 = 823
+// 8+2+"3" = 103
+//+true = 1 
